dfad09688745f0b634cba1e61219b5ab6af4a600
Yousan_O <yousan@l2tp.org>
2025-04-23 17:04:58 +0900

    Fix about supabase

--- vite-app-ant/src/utility/supabaseClient.ts
+++ vite-app-ant/src/utility/supabaseClient.ts
@@ -1,10 +1,9 @@
 import { createClient } from "@refinedev/supabase";
 
-const SUPABASE_URL = "https://iwdfzvfqbtokqetmbmbp.supabase.co";
-const SUPABASE_KEY =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzMDU2NzAxMCwiZXhwIjoxOTQ2MTQzMDEwfQ._gr6kXGkQBi9BM9dx5vKaNKYj_DJN1xlkarprGpM_fU";
+const supabaseUrl= import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 
-export const supabaseClient = createClient(SUPABASE_URL, SUPABASE_KEY, {
+export const supabaseClient = createClient(supabaseUrl, supabaseKey, {
   db: {
     schema: "public",
   },
