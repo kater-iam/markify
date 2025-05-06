32bbcfe215ac1b96fd80512a8edd722a8d606ef8
Yousan_O <yousan@l2tp.org>
2025-05-06 22:01:21 +0900

    Add

--- vite-app-ant/src/pages/images/create.tsx
+++ vite-app-ant/src/pages/images/create.tsx
@@ -1,5 +1,140 @@
-import { AntdInferencer } from "@refinedev/inferencer/antd";
+import React from "react";
+import { Create, useForm, useSelect, getValueFromEvent } from "@refinedev/antd";
+import { Form, Input, Select, Upload, DatePicker } from "antd";
+import dayjs from "dayjs";
 
 export const ImagesCreate = () => {
-    return <AntdInferencer />;
+    const { formProps, saveButtonProps, query } = useForm();
+
+    const { selectProps: profileSelectProps } = useSelect({
+        resource: "profiles",
+        optionLabel: "first_name",
+    });
+
+    return (
+        <Create saveButtonProps={saveButtonProps}>
+            <Form {...formProps} layout="vertical">
+                <Form.Item
+                    label="Profile"
+                    name={"profile_id"}
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Select {...profileSelectProps} />
+                </Form.Item>
+                <Form.Item label="File Path">
+                    <Form.Item
+                        name="file_path"
+                        getValueProps={(value) => ({
+                            fileList: [{ url: value, name: value, uid: value }],
+                        })}
+                        getValueFromEvent={getValueFromEvent}
+                        noStyle
+                        rules={[
+                            {
+                                required: true,
+                            },
+                        ]}
+                    >
+                        <Upload.Dragger
+                            listType="picture"
+                            beforeUpload={() => false}
+                        >
+                            <p className="ant-upload-text">
+                                Drag & drop a file in this area
+                            </p>
+                        </Upload.Dragger>
+                    </Form.Item>
+                </Form.Item>
+                <Form.Item label="Original Filename">
+                    <Form.Item
+                        name="original_filename"
+                        getValueProps={(value) => ({
+                            fileList: [{ url: value, name: value, uid: value }],
+                        })}
+                        getValueFromEvent={getValueFromEvent}
+                        noStyle
+                        rules={[
+                            {
+                                required: true,
+                            },
+                        ]}
+                    >
+                        <Upload.Dragger
+                            listType="picture"
+                            beforeUpload={() => false}
+                        >
+                            <p className="ant-upload-text">
+                                Drag & drop a file in this area
+                            </p>
+                        </Upload.Dragger>
+                    </Form.Item>
+                </Form.Item>
+                <Form.Item
+                    label="Name"
+                    name={["name"]}
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Width"
+                    name={["width"]}
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Height"
+                    name={["height"]}
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                >
+                    <Input />
+                </Form.Item>
+                <Form.Item
+                    label="Created At"
+                    name={["created_at"]}
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                    getValueProps={(value) => ({
+                        value: value ? dayjs(value) : undefined,
+                    })}
+                >
+                    <DatePicker />
+                </Form.Item>
+                <Form.Item
+                    label="Updated At"
+                    name={["updated_at"]}
+                    rules={[
+                        {
+                            required: true,
+                        },
+                    ]}
+                    getValueProps={(value) => ({
+                        value: value ? dayjs(value) : undefined,
+                    })}
+                >
+                    <DatePicker />
+                </Form.Item>
+            </Form>
+        </Create>
+    );
 };
